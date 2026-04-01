67455e9ba407071f87e781df84bdca39eadcff3c
baize <hubiao2000@foxmail.com>
2026-04-01 16:38:19 +0800

    feat: 添加注释并显示动态消息
    为App组件和main.jsx添加注释说明
    在App组件中添加动态消息显示功能

--- react/my-app/src/App.jsx
+++ react/my-app/src/App.jsx
@@ -1,9 +1,10 @@
-
+// 项目的根组件
 function App() {
-
+  const msg = 'hello world'
   return (
     <div className="App">
       <p>Hello React</p>
+      {msg}
     </div>
   )
 }
--- react/my-app/src/main.jsx
+++ react/my-app/src/main.jsx
@@ -1,7 +1,12 @@
+//整个项目的入口文件 从这里开始运行
+// 引入react和react-dom，react最重要的两个核心库
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+//导入项目的根组件
 import App from './App.jsx'
 
+// 创建react-dom的根实例，用于渲染react组件
+// 渲染项目的根组件到页面的root元素中
 createRoot(document.getElementById('root')).render(
     <App />
 )
