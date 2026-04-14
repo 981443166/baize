906ac17956bff02fb9aff1b4c6ce544b99ce2323
baize <hubiao2000@foxmail.com>
2026-04-14 08:55:43 +0800

    feat: 实现基础条件渲染功能
    添加逻辑与运算和三元运算符的条件渲染示例，展示不同登录状态的显示效果

--- react/react-demo/src/App.js
+++ react/react-demo/src/App.js
@@ -25,21 +25,37 @@
 // }
 
 // day1 1.1: 实现列表渲染
-const list = [
-  { id: 1, name: "vue" },
-  { id: 2, name: "react" },
-  { id: 3, name: "angulara" },
-];
-function App() {
-  return (
-    <div className="App">
-      <p>Hello React</p>
-      <ul>
-        {/* 列表渲染 */}
-        {list.map(item => <li>{item.name}</li>)}
-      </ul>
+// const list = [
+//   { id: 1, name: "vue" },
+//   { id: 2, name: "react" },
+//   { id: 3, name: "angulara" },
+// ];
+// function App() {
+//   return (
+//     <div className="App">
+//       <p>Hello React</p>
+//       <ul>
+//         {/* 列表渲染 */}
+//         {list.map(item => <li key={item.id}>{item.name}</li>)}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// day1 1.2: 实现基础条件渲染
+const isLogin = true;
+
+function App(){
+  return(
+    <div>
+      {/* {逻辑与运算} */}
+      {/* 仅当isLogin为true时，渲染<p>已登录</p> */}
+      <p>逻辑与运算</p>
+      {isLogin && <p>已登录</p>}
+      {/* {三元运算符} */}
+      {isLogin ? <p>已登录</p> : <p>未登录</p>}
     </div>
-  );
+  )
 }
 
 export default App;
