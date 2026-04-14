2a6551b769a0fe51c6c9056c67ec70962f843ec4
baize <hubiao2000@foxmail.com>
2026-04-14 09:13:30 +0800

    feat: 实现按钮点击事件处理并传递自定义参数
    添加事件处理函数handleClick，支持同时接收事件对象和自定义参数

--- react/react-demo/src/App.js
+++ react/react-demo/src/App.js
@@ -59,22 +59,53 @@
 // }
 
 // day1 1.3: 实现复杂条件渲染
-const getNameType = 0;
+// const getNameType = 0;
 
-function getName(){
-  if(getNameType === 0){
-    return <div>周杰伦</div>
-  }else if(getNameType === 1){
-    return <div>谢霆锋</div>
-  }else{
-    return <div>黎明</div>
-  }
+// function getName(){
+//   if(getNameType === 0){
+//     return <div>周杰伦</div>
+//   }else if(getNameType === 1){
+//     return <div>谢霆锋</div>
+//   }else{
+//     return <div>黎明</div>
+//   }
+// }
+
+// function App(){
+//   return(
+//     <div className="App">
+//       {getName()}
+//     </div>
+//   )
+// }
+
+// day1 1.4: 事件绑定
+// 基础事件绑定
+// function handleClick(){
+//   console.log('点击了按钮')
+// }
+// 事件参数e
+// function handleClick(e){
+//   console.log('点击了按钮', e)
+// }
+//  自定义参数
+// function handleClick(name){
+//   console.log('点击了按钮', name)
+// }
+//  同时传递事件参数e和自定义参数
+function handleClick(e, name){
+  console.log('点击了按钮', e, name)
 }
 
 function App(){
   return(
+    // 基础事件绑定
+    // <div className="App">
+    //   <button onClick={handleClick}>点击我</button>
+    // </div>
+    // 事件参数e
     <div className="App">
-      {getName()}
+      <button onClick={(e) => handleClick(e, 'baize')}>点击我</button>
     </div>
   )
 }
