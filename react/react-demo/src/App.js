7ed3863b54418ae198adf5179cdba584abd0adb9
baize <hubiao2000@foxmail.com>
2026-04-18 09:18:30 +0800

    refactor: 重构React组件结构并清理无用代码
    - 删除App.css文件及无用样式
    - 移动App组件到根目录并简化实现
    - 删除demo.js及相关注释代码
    - 清理demo/App.js中的历史示例代码

--- react/react-demo/src/App.js
+++ react/react-demo/src/App.js
@@ -1,22 +1,44 @@
 //受控绑定表单
-import { useState } from "react";
+// import { useState } from "react";
+
+// function App() {
+//   //1 首先声明一个useState状态
+//   const [value, setValue] = useState("");
+//   return (
+//     <div>
+//       <input
+//         //2 核心绑定流程
+//         //  1 通过value属性绑定raact状态
+//         value={value}
+//         //  2 通过onChange事件 通过参数e获取到用户输入的内容 再反向修改到react状态
+//         onChange={(e) => {
+//           setValue(e.target.value);
+//         }}
+//         type="text"
+//       />
+//     </div>
+//   );
+// }
+
+
+// react 获取 DOM
+import { useRef } from "react";
 
 function App() {
-  //1 首先声明一个useState状态
-  const [value, setValue] = useState("");
+  const inputRef = useRef(null);
+  const showDown = () => {
+    console.dir(inputRef.current);
+  }
   return (
     <div>
       <input
-        //2 核心绑定流程
-        //  1 通过value属性绑定raact状态
-        value={value}
-        //  2 通过onChange事件 通过参数e获取到用户输入的内容 再反向修改到react状态
-        onChange={(e) => {
-          setValue(e.target.value);
-        }}
         type="text"
+        id="myInput"
+        ref={inputRef}
       />
+      <button onClick={showDown}>获取DOM</button>
     </div>
   );
 }
 export default App;
+
