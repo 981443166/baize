f0082023adb1076b1fc1b7b27381c253dcb013b2
baize <hubiao2000@foxmail.com>
2026-04-18 14:55:49 +0800

    refactor: 重构父子组件通信示例代码
    移除旧的父子组件props传递示例，改为子组件向父组件通信的实现

--- react/react-demo/src/App.js
+++ react/react-demo/src/App.js
@@ -20,7 +20,6 @@
 //   );
 // }
 
-
 // react 获取 DOM
 // import { useRef } from "react";
 
@@ -42,19 +41,46 @@
 // }
 
 // 父传子数据
-import './App.scss'
-function Son(props){
-  console.log(props.name);
-  return <div className="son">this is son component,{props.name}</div>
-}
+// import './App.scss'
+// //props可以传递任意数据，但是在子组件里面只能读取，不能修改父组件的状态
+// function Son(props){
+//   console.log(props.name);
+//   return <div className="son">this is son component,{props.name},{props.children}</div>
+// }
 
-function App() {
-  const name = "this is parent component";
+// function App() {
+//   const name = "this is parent component";
+//   return (
+//     // <div>
+//     //   <Son name={name} />
+//     // </div>
+
+//     //特殊的props  children  在子组件为标签嵌套一个标签
+//     <Son name={name}>
+//       <p>这是一个段落</p>
+//     </Son>
+//   );
+// }
+
+// 子传父
+// 就是在子组件里面调用父组件的函数
+// 子组件
+function Son() {
+  const sonMsg = "this is son component";
   return (
     <div>
-      <Son name={name} />
+      <p>this is son</p>
+      <button>send</button>
+    </div>
+  )
+}
+//父组件
+function App() {
+  return (
+    <div>
+      <p>this is parent</p>
+      <Son />
     </div>
   );
 }
 export default App;
-
